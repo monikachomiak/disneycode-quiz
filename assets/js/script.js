2924228f233468918c6b486c193d0ba508009d99
ernie <monikachomiak@gmail.com>
2022-04-16 19:33:25 -0500

    adding html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,26 @@ function countdown() {
 
 // Intervals
 var timeInterval = setInterval(function () {
-    //As long as the timeLeft is greater than 1
+    // As long as the timeLeft is greater than 1
     if (timeLeft > 1) {
-        //Set the textContent to show the number of seconds remaining
-        timeInterval.textContent = timeLeft + ' seconds remaining '
+        //Set the textContent of timerDec to show the number of seconds remaining
+        timerDec.textContent = timeLeft + ' seconds remaining'
+    // Decrease timeLeft by 1
+        timeLeft--;
+    }
+    else if (timeLeft === 1) {
+        // When timeLeft is equal to 1, rename ' seconds ' to ' second'
+        timerDec.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+    }
+    else {
+    // Once timeLeft gets to 0, set timerDec to an empty string
+        timerDec.textContent = '';
+    // Use clearInterval to stop the timer
+        clearInterval(timeInterval);
+    // displayMessage Function
+    displayMessage();
+
     }
 }
 }
