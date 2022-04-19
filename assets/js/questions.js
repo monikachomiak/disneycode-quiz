02d2bf9f2c664c6b6b709b6afbaa978a917e9c69
ernie <monikachomiak@gmail.com>
2022-04-19 10:32:37 -0500

    adding css to the header

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+    prompt:"In Finding Nemo, what does Dory tell Marlin to do when he gets discouraged?", 
+    choices: ["1.) Just keep laughing" , "2.) Just keep swimming" , "3.) Just keep breathing" , "4.) Shut up"],
+    answer: "2.) Just keep swimming"
+    },
+
+    {
+    prompt:"What is the name of Aladdin’s best friend who is a monkey?",
+    choices:["1.) Pablo" , "2.) Ali" , "3.) Simon" , "4.) Abu"],
+    answer:"4.) Abu"
+    },
+
+    {
+    prompt:"In Turning Red, what type of animal does Mei-Mei turn into?",
+    choices:["1.) Cat", "2.) Lizard", "3.) Red Panda", "4.) Bear"],
+    answer: "3.) Red Panda"
+    }
+                ]
+
+
+
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,39 @@
-// Timer that counts down from 10
-function countdown() {
+var timerEl = document.querySelector("#timer")
+
+
+
+// Timer that counts down from 10 --> change to startquiz & add nextquestion function
+function startQuiz() {
     var timeLeft = 10;
 
 // Intervals
 var timeInterval = setInterval(function () {
-    // As long as the timeLeft is greater than 1
+    // If the timeLeft is greater than 1
     if (timeLeft > 1) {
-        //Set the textContent of timerDec to show the number of seconds remaining
-        timerDec.textContent = timeLeft + ' seconds remaining'
+        //Set the textContent of timerEl to show the number of seconds remaining
+        timerEl.textContent = timeLeft + ' seconds remaining'
+        console.log(timeLeft);
     // Decrease timeLeft by 1
         timeLeft--;
     }
     else if (timeLeft === 1) {
         // When timeLeft is equal to 1, rename ' seconds ' to ' second'
-        timerDec.textContent = timeLeft + ' second remaining';
+        timerEl.textContent = timeLeft + ' second remaining';
         timeLeft--;
     }
     else {
-    // Once timeLeft gets to 0, set timerDec to an empty string
-        timerDec.textContent = '';
+    // Once timeLeft gets to 0, set timerEl to an empty string
+        timerEl.textContent = '';
     // Use clearInterval to stop the timer
         clearInterval(timeInterval);
     // displayMessage Function
     displayMessage();
 
     }
+},1000)
+
 }
-}
+
+
+
+startQuiz()
